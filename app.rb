c573f715ff4244ff6011915adec8a60901ff60c1
dsunjic <114637883+DylanSunj@users.noreply.github.com>
2023-10-17 22:36:01 +0000

    Made the website dynamic

--- app.rb
+++ app.rb
@@ -52,3 +52,18 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dice/:zebra/:sides") do
+  @num_die = params.fetch("zebra").to_i
+  @sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_die.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
